169ccdfb665d851215befe931609cf8cb33ce64c
henning <henning@bwp-codes.de>
2022-04-16 21:03:36 +0200

    updated readme & author

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 
 ## Usage
 
-Install as regular Wordpress mu-plugin or `composer require bwp-codes/oxygen-cli`for your Composer based Wordpress Setup (e.g. roots/bedrock).
+Install as regular Wordpress mu-plugin or `composer require bwp-codes/oxygen-cli` for your Composer based Wordpress Setup (e.g. roots/bedrock).
+
+## Available Commands
+
+-   `wp oxygen sign-shortcodes` to sign shortcodes
+-   `wp oxygen css-cache` to regenerate the CSS cache
+
+### Credit
+
+WP CLI commands based on the work of https://github.com/artifact-modules/command (MIT license)
--- oxygen-cli-commands.php
+++ oxygen-cli-commands.php
@@ -1,18 +1,23 @@
 <?php
 /**
- * Plugin Name:  Oxygen WP CLI Commands
+ * Plugin Name:  Oxygen CLI
  * Plugin URI:   https://github.com/artifact-modules/command
- * Description:  Collection of WP-CLI commands.
- * Version:      0.0.1
- * Author:       dPlugins
- * Author URI:   https://dplugins.com/
+ * Description:  Collection of WP-CLI commands for Oxygen Builder (https://oxygenbuilder.com/).
+ * Version:      0.0.3
+ * Author:       BWP Codes
+ * Author URI:   https://bwp-codes.de/
  * License:      MIT License
+ *
+ * @package Oxygen CLI
  */
 
+/**
+ * Check if WP CLI is running.
+ */
 function is_cli_running() {
-    return defined( 'WP_CLI' ) && WP_CLI;
+	return defined( 'WP_CLI' ) && WP_CLI;
 }
 
 if ( is_cli_running() ) {
-    require_once plugin_dir_path( __FILE__ ) . '/commands/commands.php';
+	require_once plugin_dir_path( __FILE__ ) . '/commands/commands.php';
 }
